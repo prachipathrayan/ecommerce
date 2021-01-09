258a91ea757ed1fbf246cfb7ff77c0144ef12b64
prachipathrayan <33428054+prachipathrayan@users.noreply.github.com>
2021-01-08 21:55:05 -0800

    refactor : auth router

---
+++ basic_ecommerce/src/routes/auth.ts
@@ -0,0 +1,68 @@
+import { Router as route } from 'express';
+import { AuthService } from '../services/authService';
+import { nest } from '../utils';
+import logger from '../utils/logger';
+
+const router = route();
+
+
+
+// path /auth/login
+router.post('/login', async (req, res) => {
+    const authService = new AuthService();
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.json({
+            data: null,
+            error: 'invalid payload',
+        });
+    }
+    let err : Error;
+    let token : string;
+    [err, token] = await nest(authService.logInAndGenerateToken({
+        email,
+        password,
+    }));
+    if(err){
+        logger.error('Error while logging in the user', {Error : err});
+        return res.json({ error: err.message, data: null });
+    }
+    return res.json({
+        data: {
+            jwtToken: token,
+        },
+        error: null,
+    });
+});
+
+router.post('/signup', async (req, res) => {
+    const authService = new AuthService();
+    const { username, name, type, email, password } = req.body;
+    if (!username || !name || !email || !password) {
+        return res.json({
+            data: null,
+            error: 'Invalid payload',
+        });
+    }
+    const [err, token] = await nest(
+        authService.signUpAndGenerateToken({
+            username,
+            name,
+            email,
+            password,
+            type,
+        })
+    );
+    if (err) {
+        logger.error('Error while signing up the user', {Error : err});
+        return res.json({ error: err.message, data: null });
+    }
+    return res.json({
+        data: {
+            jwtToken: token,
+        },
+        error: null,
+    });
+});
+
+export default router;
--- basic_ecommerce/src/routes/index.ts
+++ basic_ecommerce/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserRouter from './Users';
+import AuthRouter from './auth'
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
+router.use('/auth', AuthRouter);
 router.use('/users', UserRouter);
 
 // Export the base-router
